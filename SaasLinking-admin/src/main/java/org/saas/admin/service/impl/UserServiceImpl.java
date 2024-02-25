fb3c0e074b1595afa8233b84ea030fd83d114e87
elysium-w <wya10241110@163.com>
2024-02-25 17:05:33 +0800

    feature:使用分布式锁完善大量用户请求注册功能

--- SaasLinking-admin/src/main/java/org/saas/admin/service/impl/UserServiceImpl.java
+++ SaasLinking-admin/src/main/java/org/saas/admin/service/impl/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package org.saas.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.saas.admin.common.conversion.exception.ClientException;
 import org.saas.admin.dao.entity.UserDO;
 import org.saas.admin.dao.mapper.UserMapper;
@@ -16,13 +17,14 @@ import org.saas.admin.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import static org.saas.admin.common.enums.UserErrorCode.USERNAME_EXIST;
-import static org.saas.admin.common.enums.UserErrorCode.USER_SAVE_ERROR;
+import static org.saas.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
+import static org.saas.admin.common.enums.UserErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> query= Wrappers.lambdaQuery(UserDO.class)
@@ -46,9 +48,17 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (hashUsername(requestParam.getUsername())){
             throw new ClientException(USERNAME_EXIST);
         }
-        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam,UserDO.class));
-        if (inserted < 1){
-            throw new ClientException(USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try{
+            if (lock.tryLock()){
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam,UserDO.class));
+                if (inserted < 1){
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+            }
+            throw new ClientException(USER_EXIST);
+        }finally {
+            lock.unlock();
         }
     }
 
