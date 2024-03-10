c45826f0a44136805ccde3fb5216eb6828915d22
elysium-w <wya10241110@163.com>
2024-03-10 17:16:42 +0800

    feature：用户上下文封装

---
+++ SaasLinking-admin/src/main/java/org/saas/admin/config/UserConfiguration.java
@@ -0,0 +1,27 @@
+package org.saas.admin.config;
+
+import org.saas.admin.common.biz.user.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * 用户配置自动装配
+ */
+@Configuration
+public class UserConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        registration.addUrlPatterns("/*");
+        registration.setOrder(0);
+        return registration;
+    }
+}
