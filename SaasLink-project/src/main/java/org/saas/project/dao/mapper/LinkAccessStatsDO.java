171b1099aeeee04cb39dc931fee44f29d9d56bbb
elysium-w <wya10241110@163.com>
2024-03-19 23:17:56 +0800

    feature:短链接监控模块骨架搭建

---
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkAccessStatsDO.java
@@ -0,0 +1,10 @@
+package org.saas.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 短链接基础访问监控持久层
+ */
+public interface LinkAccessStatsDO extends BaseMapper<LinkAccessStatsDO> {
+
+}
