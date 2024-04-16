e717e38ac74ff2f28c386f7643b7767971c7dc45
elysium-w <wya10241110@163.com>
2024-04-16 11:40:44 +0800

    feature:开发新增短链接用户地区访问记录

--- SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkLocaleStatsDO.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkLocaleStatsDO.java
@@ -2,5 +2,50 @@ package org.saas.project.dao.entity;
 
 import org.saas.project.common.database.BaseDO;
 
+import java.util.Date;
+
+/**
+ * 短链接用户访问地点实体
+ */
 public class LinkLocaleStatsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 省份名称
+     */
+    private String province;
+
+    /**
+     * 市名称
+     */
+    private String city;
+
+    /**
+     * 城市编码
+     */
+    private String adcode;
+
+    /**
+     * 国家标识
+     */
+    private String country;
 }
--- SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkLocaleStatsMapper.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -1,4 +1,15 @@
 package org.saas.project.dao.mapper;
 
-public interface LinkLocaleStatsMapper {
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.saas.project.dao.entity.LinkLocaleStatsDO;
+
+/**
+ * 短链接访问地区监控记录
+ */
+public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
+    /**
+     * 新增访问位置监控记录
+     */
+    void shortLinkLocaleState(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
 }
