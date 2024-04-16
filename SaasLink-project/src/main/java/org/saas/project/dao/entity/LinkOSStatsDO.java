1a92d48cabd5d4172023801d29f288d0a575ca00
elysium-w <wya10241110@163.com>
2024-04-16 11:41:04 +0800

    feature:开发新增短链接用户操作系统访问记录

--- SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkOSStatsDO.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkOSStatsDO.java
@@ -1,17 +1,45 @@
 package org.saas.project.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.saas.project.common.database.BaseDO;
 
+import java.util.Date;
+
 /**
- * 用户操作系统访问实体
+ * 短链接用户操作系统访问实体
  */
+@TableName("t_link_os_stats")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LinkOSStatsDO extends BaseDO {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 操作系统
+     */
+    private String os;
 }
--- SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkOSStatsMapper.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkOSStatsMapper.java
@@ -1,4 +1,21 @@
 package org.saas.project.dao.mapper;
 
-public interface LinkOSStatsMapper {
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.saas.project.dao.entity.LinkOSStatsDO;
+
+/**
+ * 短链接访问操作系统监控记录
+ */
+public interface LinkOSStatsMapper extends BaseMapper<LinkOSStatsDO> {
+    /**
+     * 新增短链接访问操作系统监控
+     */
+
+    @Insert("INSERT INTO " +
+            "t_link_os_stats (full_short_url, date, cnt, os, create_time, update_time, del_flag) " +
+            "VALUES( #{linkOsStats.fullShortUrl}, #{linkOsStats.date}, #{linkOsStats.cnt}, #{linkOsStats.os}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkOsStats.cnt};")
+    void shortLinkOSState(@Param("linkOsStats")LinkOSStatsDO linkOSStatsDO);
 }
