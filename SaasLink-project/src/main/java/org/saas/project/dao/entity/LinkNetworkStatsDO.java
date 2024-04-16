b41082e00f4ebbfcdbe3035661a1bff7fee01c14
elysium-w <wya10241110@163.com>
2024-04-16 11:40:55 +0800

    feature:开发新增短链接用户网络访问记录

--- SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkNetworkStatsDO.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkNetworkStatsDO.java
@@ -1,4 +1,46 @@
 package org.saas.project.dao.entity;
 
-public class LinkNetworkStatsDO {
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.saas.project.common.database.BaseDO;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * 短链接用户访问网络实体
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_network_stats")
+public class LinkNetworkStatsDO extends BaseDO {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 访问网络
+     */
+    private String network;
 }
--- SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkNetworkStatsMapper.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkNetworkStatsMapper.java
@@ -1,4 +1,21 @@
 package org.saas.project.dao.mapper;
 
-public interface LinkNetworkStatsMapper {
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.saas.project.dao.entity.LinkNetworkStatsDO;
+
+/**
+ *短链接访问网络监控记录
+ */
+public interface LinkNetworkStatsMapper extends BaseMapper<LinkNetworkStatsDO> {
+    /**
+     * 新增短链接访问网络监控记录
+     */
+    @Insert("INSERT INTO " +
+            "t_link_network_stats (full_short_url, date, cnt, network, create_time, update_time, del_flag) " +
+            "VALUES( #{linkNetworkStats.fullShortUrl}, #{linkNetworkStats.date}, #{linkNetworkStats.cnt}, #{linkNetworkStats.network}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkNetworkStats.cnt};")
+    void shortLinkNetworkState(@Param("linkNetworkStats")LinkNetworkStatsDO linkNetworkStatsDO);
+
 }
