bda9d6cb9555da342c8d67e9f40c3f15e548d5e6
elysium-w <wya10241110@163.com>
2024-04-16 15:35:35 +0800

    feature:根据短链接查询访问地址信息

--- SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkLocaleStatsMapper.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -1,8 +1,13 @@
 package org.saas.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.saas.project.dao.entity.LinkLocaleStatsDO;
+import org.saas.project.dto.req.ShortLinkStatsReqDTO;
+
+import java.util.List;
 
 /**
  * 短链接访问地区监控记录
@@ -11,5 +16,29 @@ public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
     /**
      * 新增访问位置监控记录
      */
+    @Insert("INSERT INTO " +
+            "t_link_locale_stats (full_short_url, date, cnt, country, province, city, adcode, create_time, update_time, del_flag) " +
+            "VALUES( #{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.date}, #{linkLocaleStats.cnt}, #{linkLocaleStats.country}, #{linkLocaleStats.province}, #{linkLocaleStats.city}, #{linkLocaleStats.adcode}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkLocaleStats.cnt};")
     void shortLinkLocaleState(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
+
+    /**
+     * 根据短链接获取指定日期内短链接位置数据
+     */
+    @Select("SELECT " +
+            "    tlls.province, " +
+            "    SUM(tlls.cnt) AS cnt " +
+            "FROM " +
+            "    t_link tl INNER JOIN " +
+            "    t_link_locale_stats tlls ON tl.full_short_url = tlls.full_short_url " +
+            "WHERE " +
+            "    tlls.full_short_url = #{param.fullShortUrl} " +
+            "    AND tl.gid = #{param.gid} " +
+            "    AND tl.del_flag = '0' " +
+            "    AND tl.enable_status = #{param.enableStatus} " +
+            "    AND tlls.date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    tlls.full_short_url, tl.gid, tlls.province;")
+    List<LinkLocaleStatsDO> listLocaleByShortLink(@Param("param")ShortLinkStatsReqDTO requestParam);
+
 }
