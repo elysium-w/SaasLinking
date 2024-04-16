2f937a93ce965fbf09583cfccfe192ef0d3ae562
elysium-w <wya10241110@163.com>
2024-04-16 10:40:48 +0800

    feature:短链接监控模块新增浏览器监控记录

--- SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkBrowserStatsDO.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkBrowserStatsDO.java
@@ -8,8 +8,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.saas.project.common.database.BaseDO;
 
+import java.util.Date;
+
 /**
- * 用户浏览器访问实体
+ * 短链接用户浏览器访问实体
  */
 @Data
 @Builder
@@ -17,4 +19,29 @@ import org.saas.project.common.database.BaseDO;
 @AllArgsConstructor
 @TableName("t_link_browser_stats")
 public class LinkBrowserStatsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
 }
--- SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkBrowserStatsMapper.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkBrowserStatsMapper.java
@@ -1,4 +1,21 @@
 package org.saas.project.dao.mapper;
 
-public interface LinkBrowserStatsMapper {
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.saas.project.dao.entity.LinkBrowserStatsDO;
+
+/**
+ * 短链接监控浏览器访问持久层
+ */
+public interface LinkBrowserStatsMapper extends BaseMapper<LinkBrowserStatsDO> {
+    /**
+     * 记录浏览器监控记录
+     */
+    @Insert("INSERT INTO " +
+            "t_link_browser_stats (full_short_url, date, cnt, browser, create_time, update_time, del_flag) " +
+            "VALUES( #{linkBrowserStats.fullShortUrl}, #{linkBrowserStats.date}, #{linkBrowserStats.cnt}, #{linkBrowserStats.browser}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkBrowserStats.cnt};")
+    void shortLinkBrowserState(@Param("linkBrowserStats") LinkBrowserStatsDO linkBrowserStatsDO);
+
 }
