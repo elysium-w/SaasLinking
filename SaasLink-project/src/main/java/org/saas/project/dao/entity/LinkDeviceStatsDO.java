26cf612861c95bde8976486f71c9b84bc81e05ae
elysium-w <wya10241110@163.com>
2024-04-16 11:40:20 +0800

    feature:开发短链接用户设备访问监控记录

--- SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkDeviceStatsDO.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/entity/LinkDeviceStatsDO.java
@@ -1,4 +1,46 @@
 package org.saas.project.dao.entity;
 
-public class LinkDeviceStatsDO {
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.codehaus.groovy.transform.trait.Traits;
+import org.saas.project.common.database.BaseDO;
+
+import java.util.Date;
+
+/**
+ * 短链接用户访问设备实体
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_device_stats")
+public class LinkDeviceStatsDO extends BaseDO {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 访问设备
+     */
+    private String device;
 }
--- SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkDeviceStatsMapper.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkDeviceStatsMapper.java
@@ -1,4 +1,22 @@
 package org.saas.project.dao.mapper;
 
-public interface LinkDeviceStatsMapper {
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.saas.project.dao.entity.LinkDeviceStatsDO;
+
+/**
+ * 短链接用户访问设备监控记录
+ */
+public interface LinkDeviceStatsMapper extends BaseMapper<LinkDeviceStatsDO> {
+    /**
+     * 访问设备监控记录
+     * @param linkDeviceStatsDO
+     */
+
+    @Insert("INSERT INTO " +
+            "t_link_device_stats (full_short_url, date, cnt, device, create_time, update_time, del_flag) " +
+            "VALUES( #{linkDeviceStats.fullShortUrl}, #{linkDeviceStats.date}, #{linkDeviceStats.cnt}, #{linkDeviceStats.device}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkDeviceStats.cnt};")
+    void shortLinkDeviceState(@Param("linkDeviceStats") LinkDeviceStatsDO linkDeviceStatsDO);
 }
