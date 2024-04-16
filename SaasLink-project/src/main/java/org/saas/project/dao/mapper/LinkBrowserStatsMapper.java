3d6addf17f7f34241d8f0ce2605df2c7207baaa5
elysium-w <wya10241110@163.com>
2024-04-16 18:10:06 +0800

    feature:根据短链接查询浏览器访问监控

--- SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkBrowserStatsMapper.java
+++ SaasLink-project/src/main/java/org/saas/project/dao/mapper/LinkBrowserStatsMapper.java
@@ -3,7 +3,12 @@ package org.saas.project.dao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.saas.project.dao.entity.LinkBrowserStatsDO;
+import org.saas.project.dto.req.ShortLinkStatsReqDTO;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 短链接监控浏览器访问持久层
@@ -17,5 +22,29 @@ public interface LinkBrowserStatsMapper extends BaseMapper<LinkBrowserStatsDO> {
             "VALUES( #{linkBrowserStats.fullShortUrl}, #{linkBrowserStats.date}, #{linkBrowserStats.cnt}, #{linkBrowserStats.browser}, NOW(), NOW(), 0) " +
             "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkBrowserStats.cnt};")
     void shortLinkBrowserState(@Param("linkBrowserStats") LinkBrowserStatsDO linkBrowserStatsDO);
+
+    /**
+     * 根据短链接获取指定日期内浏览器监控数据
+     */
+    @Select("SELECT " +
+            "    tlbs.browser, " +
+            "    SUM(tlbs.cnt) AS count " +
+            "FROM " +
+            "    t_link tl INNER JOIN " +
+            "    t_link_browser_stats tlbs ON tl.full_short_url = tlbs.full_short_url " +
+            "WHERE " +
+            "    tlbs.full_short_url = #{param.fullShortUrl} " +
+            "    AND tl.gid = #{param.gid} " +
+            "    AND tl.del_flag = '0' " +
+            "    AND tl.enable_status = #{param.enableStatus} " +
+            "    AND tlbs.date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    tlbs.full_short_url, tl.gid, tlbs.browser;")
+    List<HashMap<String, Object>> listBrowserStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 根据分组获得短链接浏览器监控数据
+     */
+//    List<HashMap<String,Object>> listBrowserStatsByGroup(@Param("param") )
 
 }
